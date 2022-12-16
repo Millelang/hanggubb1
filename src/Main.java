d029b09f5f56203c98b939d280b44cf11f300b1e
Millelang <Milton.langstrom@elev.ga.ntig.se>
2022-12-16 11:05:05 +0100

    Gjorde en funktion som skapar en string med lika många "-" som ordet har bokstäver

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.concurrent.ThreadLocalRandom;
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!2");
+    public static void main(String[] args) throws FileNotFoundException {
+        String ord = Milton.test();
+        String shh = Milton.gissaord(ord);
+        System.out.println(ord);
+        System.out.println(shh);
     }
 }
--- src/Milton.java
+++ src/Milton.java
@@ -5,17 +5,31 @@ import java.util.concurrent.ThreadLocalRandom;
 
 
 public class Milton {
-    public static String test(String[] args) throws FileNotFoundException {
+    public static String test() throws FileNotFoundException {
         Scanner in = new Scanner(new File("src/svenska-ord.txt"));
         String[] ord = new String[122312];
         int x;
-        for (x = 0; x < 122312; x++) {
+        for (x = 0; x < 300; x++) {
             ord[x] = in.nextLine();
         }
-        int randomNum = ThreadLocalRandom.current().nextInt(0,  122312+ 1);
-        System.out.println(ord[randomNum]);
+        int randomNum = ThreadLocalRandom.current().nextInt(0,  300+ 1);
         return ord[randomNum];
         }
+
+    public static String gissaord(String x) throws FileNotFoundException {
+        String ord = x;
+        int antal = ord.length();
+        int u = 0;
+        char[] hemlig = new char[antal];
+        for (u=0;u<antal;u++ ) {
+            hemlig[u] = '-';
         }
+        String superhemlig = String.valueOf(hemlig);
+        return superhemlig;
+        }
+
+    }
+
+
 
 
