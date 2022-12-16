960847c5b66f11d8c6b27ba14096b934e0895f2c
Nn0el <noel.johansson@elev.ga.ntig.se>
2022-12-16 11:04:09 +0100

    Startade filen Skapat Variabler och scanner Börjat på for loop

--- src/Noel.java
+++ src/Noel.java
@@ -1,6 +1,17 @@
-public class Noel{
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.concurrent.ThreadLocalRandom;
 
-        public static void main(String[] args){
+public class Noel{
+// skapa respons till personens gissningar, om det är rätt eller fel
+        public static String main(String x, String y){
+                Scanner in = new Scanner(System.in);
+                String gissning = in.next();
+                int antal = 0;
+                for (x=0)
+
+                System.out.println();
 
 
 
