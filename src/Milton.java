987e305d997a953f203f1c64fb88f17229c519e5
Millelang <Milton.langstrom@elev.ga.ntig.se>
2023-01-17 15:16:00 +0100

    Skapade en funktion som (nästan) funkar som läser in en bokstav och kollar om det är rätt eller fel och om det är rätt så placerar den bokstaven på den platsen och om det är fel så placerar den ett "-" på den platsen

--- src/Milton.java
+++ src/Milton.java
@@ -12,22 +12,57 @@ public class Milton {
         for (x = 0; x < 300; x++) {
             ord[x] = in.nextLine();
         }
-        int randomNum = ThreadLocalRandom.current().nextInt(0,  300+ 1);
+        int randomNum = ThreadLocalRandom.current().nextInt(0, 300 + 1);
         return ord[randomNum];
-        }
+    }
 
     public static String gissaord(String x) throws FileNotFoundException {
         String ord = x;
         int antal = ord.length();
         int u = 0;
         char[] hemlig = new char[antal];
-        for (u=0;u<antal;u++ ) {
+        for (u = 0; u < antal; u++) {
             hemlig[u] = '-';
         }
         String superhemlig = String.valueOf(hemlig);
         return superhemlig;
-        }
+    }
 
+    public static String  gissning(String y) throws FileNotFoundException {
+        int ordlangd = y.length();
+        char[] gissningen = new char[ordlangd];
+        String[] svar = new String[9];
+        String[] superhemlig = new String[9];
+        int fel = 0;
+        int superfel = 0;
+        char[] bokstaven = new char[9];
+        int z;
+        int k = 0;
+        int g = 0;
+        int x = 0;
+        while ( g < 9 && y != svar[g] ) {
+            bokstaven[g] = Noel.lasInEnBokstav();
+            for (z = 0; z < ordlangd; z++) {
+                if (x == y.charAt(z)) {
+                    gissningen[z] = bokstaven[g];
+
+                } else {
+                    gissningen[z] = '-';
+                    fel++;
+
+                }
+                if (fel == ordlangd) {
+                    superfel++;
+                } else {
+                    superhemlig[g] = String.valueOf(gissningen);
+                    svar[g] = superhemlig[g];
+                }
+            }
+            g++;
+        }
+        return svar[8];
+
+    }
     }
 
 
